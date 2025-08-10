557a622725c8000b93260673091b5d59d4c6192d
VaDKo61 <vadkoo@mail.ru>
2025-08-10 16:03:20 +0300

    add admin models

--- mis/app/admin.py
+++ mis/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Clinic)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Consultation)
